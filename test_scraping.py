325cef9f6a2623332743899d6327215307e64e9c
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-11 17:56:26 +0200

    Refactoring code warnings

--- test_scraping.py
+++ test_scraping.py
@@ -1,7 +1,8 @@
 # pylint: disable=missing-docstring, invalid-name
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("pages/carrot.html"), "html.parser")
+with open("pages/carrot.html", encoding="utf-8") as html_page:
+    soup = BeautifulSoup(html_page, "html.parser")
 
 for recipe in soup.find_all('p', class_= 'recipe-name'):
     print(recipe.text)
